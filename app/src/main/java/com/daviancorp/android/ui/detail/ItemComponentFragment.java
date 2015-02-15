66841b435ef1f0efa6128cd48bb0b3c33148c51a
jaysonthepirate <jayson.the.pirate@gmail.com>
2015-02-15 12:06:23 -0800

    Fixed case where items->uses page was letting you click on decorations. (cherry picked from commit cd127d3)

--- app/src/main/java/com/daviancorp/android/ui/detail/ItemComponentFragment.java
+++ app/src/main/java/com/daviancorp/android/ui/detail/ItemComponentFragment.java
@@ -90,10 +90,10 @@ public class ItemComponentFragment extends ListFragment implements
 			i = new Intent(getActivity(), ArmorDetailActivity.class);
 			i.putExtra(ArmorDetailActivity.EXTRA_ARMOR_ID, tagId);
 		}
-//		else if (tagId >= S.SECTION_DECORATIONS) {
+		else if (tagId >= S.SECTION_DECORATIONS) {
 //			i = new Intent(getActivity(), DecorationDetailActivity.class); //TODO reenable when decorations are complete.
 //			i.putExtra(DecorationDetailActivity.EXTRA_DECORATION_ID, tagId);
-//		}
+		}
 		else {
 			i = new Intent(getActivity(), ItemDetailActivity.class);
 			i.putExtra(ItemDetailActivity.EXTRA_ITEM_ID, tagId);
