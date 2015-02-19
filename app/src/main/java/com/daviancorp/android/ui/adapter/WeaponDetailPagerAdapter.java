58aa90025ac27014b4d276cf374979bffd3845fc
jaysonthepirate <jayson.the.pirate@gmail.com>
2015-02-19 00:28:42 -0800

    Removed hard id range dependency in WeaponDetailPagerAdapter.java

--- app/src/main/java/com/daviancorp/android/ui/adapter/WeaponDetailPagerAdapter.java
+++ app/src/main/java/com/daviancorp/android/ui/adapter/WeaponDetailPagerAdapter.java
@@ -1,10 +1,12 @@
 package com.daviancorp.android.ui.adapter;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 import com.daviancorp.android.data.database.S;
+import com.daviancorp.android.data.database.DataManager;
 import com.daviancorp.android.ui.detail.ComponentListFragment;
 import com.daviancorp.android.ui.detail.WeaponBladeDetailFragment;
 import com.daviancorp.android.ui.detail.WeaponBowDetailFragment;
@@ -14,27 +16,30 @@ import com.daviancorp.android.ui.detail.WeaponTreeFragment;
 public class WeaponDetailPagerAdapter extends FragmentPagerAdapter {
 	
 	private long weaponId;
+    private Context mcontext;
 
-	public WeaponDetailPagerAdapter(FragmentManager fm, long id) {
+	public WeaponDetailPagerAdapter(FragmentManager fm, Context context, long id) {
 		super(fm);
 		this.weaponId = id;
+        this.mcontext = context;
 	}
 
 	@Override
 	public Fragment getItem(int index) {
 
+        String wtype = DataManager.get(mcontext).getWeapon(weaponId).getWtype();
+
 		switch (index) {
 		case 0:
-			// Weapon detail
-			if (weaponId < S.SECTION_BOWGUN) {
-				return WeaponBladeDetailFragment.newInstance(weaponId);
-			}
-			else if (weaponId < S.SECTION_BOW) {
-				return WeaponBowgunDetailFragment.newInstance(weaponId);
-			}
-			else {
-				return WeaponBowDetailFragment.newInstance(weaponId);
-			}
+            switch(wtype){
+                case "Light Bowgun":
+                case "Heavy Bowgun":
+                    return WeaponBowgunDetailFragment.newInstance(weaponId);
+                case "Bow":
+                    return WeaponBowDetailFragment.newInstance(weaponId);
+                default:
+                    return WeaponBladeDetailFragment.newInstance(weaponId);
+            }
 		case 1:
 			// Weapon tree
 			return WeaponTreeFragment.newInstance(weaponId);
