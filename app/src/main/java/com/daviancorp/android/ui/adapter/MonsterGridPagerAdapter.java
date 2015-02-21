07e18d5ba1b3725b76343eabd9201631023033cf
Matthew Wall <matttwall@gmail.com>
2015-02-21 15:04:13 -0600

    Fix monster tab order issue, fix weapon border

--- app/src/main/java/com/daviancorp/android/ui/adapter/MonsterGridPagerAdapter.java
+++ app/src/main/java/com/daviancorp/android/ui/adapter/MonsterGridPagerAdapter.java
@@ -17,14 +17,14 @@ public class MonsterGridPagerAdapter extends FragmentPagerAdapter {
 
 		switch (index) {
 		case 0:
-			// Monster grid (all monsters)
-			return MonsterGridFragment.newInstance(null);
+            // Monster grid (large monsters)
+            return MonsterGridFragment.newInstance("Large");
 		case 1:
 			// Monster grid (small monsters)
 			return MonsterGridFragment.newInstance("Small");
 		case 2:
-			// Monster grid (large monsters)
-			return MonsterGridFragment.newInstance("Large");
+            // Monster grid (all monsters)
+            return MonsterGridFragment.newInstance(null);
 		default:
 			return null;
 		}
