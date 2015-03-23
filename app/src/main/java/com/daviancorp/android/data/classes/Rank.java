4b554505e0f24dd2d025b2f07a59c7375957c094
Zach Day! <zachdayz@gmail.com>
2015-03-22 21:36:58 -0400

    Implemented filtering for the Armor List.

---
+++ app/src/main/java/com/daviancorp/android/data/classes/Rank.java
@@ -0,0 +1,39 @@
+package com.daviancorp.android.data.classes;
+
+public enum Rank {
+    NONE("None", 1, 10),
+    LOW("Low", 1, 3),
+    HIGH("High", 4, 7),
+    G("G", 8, 10);
+
+    int armorMinimumRarity;
+    int armorMaximumRarity;
+    String properName;
+
+    private Rank(String properName) {
+        this.properName = properName;
+    }
+
+    private Rank(String properName, int armorMinimumRarity, int armorMaximumRarity) {
+        this(properName);
+        this.armorMinimumRarity = armorMinimumRarity;
+        this.armorMaximumRarity = armorMaximumRarity;
+    }
+
+    public int getArmorMinimumRarity() {
+        return armorMinimumRarity;
+    }
+
+    public int getArmorMaximumRarity() {
+        return armorMaximumRarity;
+    }
+
+    public String getProperName() {
+        return properName;
+    }
+
+    @Override
+    public String toString() {
+        return getProperName();
+    }
+}
